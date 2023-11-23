f6dca4fa195d72c2fd0d63fe6189dcbf9d173fda
DESKTOP-MAO03DS\Puesto4 <pablo112891@gmail.com>
2023-11-23 09:41:40 +0100

    comentarios

--- ApiC#/Program.cs
+++ ApiC#/Program.cs
@@ -3,21 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Agregar controladores a la aplicación
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Agregar servicios para la generación de documentación Swagger/OpenAPI https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-
+// Configurar y agregar el contexto de base de datos a través de Entity Framework Core
 builder.Services.AddDbContext<ApiDBContexto>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionStr"));
 });
 
 var app = builder.Build();
-
+// Aplicar migraciones al iniciar la aplicación para garantizar que la base de datos esté actualizada
 using (var scope = app.Services.CreateScope())
 {
     var contexto = scope.ServiceProvider.GetRequiredService<ApiDBContexto>();
@@ -27,7 +27,7 @@ using (var scope = app.Services.CreateScope())
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{   // Habilitar Swagger para entornos de desarrollo
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -35,7 +35,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+// Mapear los controladores en la aplicación
 app.MapControllers();
 
 app.Run();
